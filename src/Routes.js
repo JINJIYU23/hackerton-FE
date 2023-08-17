f9a395030fcf18617148b87313c7ccf4d4b0c951
jeongjiyu <dhkswksla22@gmail.com>
2023-08-17 21:45:23 +0900

    0817

--- src/Routes.js
+++ src/Routes.js
@@ -4,20 +4,17 @@ import Main from "./pages/Main/Main";
 import Nav from './components/Nav/Nav';
 import Login from "./pages/Login/Login";
 import Join from "./pages/Join/Join";
-import { RecoilRoot } from "recoil";
 
 const Rout = () => {
     return (
         <div className="Rout">
             <BrowserRouter>
-                <RecoilRoot>
             <Nav />
             <Routes>
                 <Route path="/" element={<Main />}></Route>
                 <Route path="/member/login" element={<Login />}></Route>
                 <Route path="/member/join" element={<Join />}></Route>
             </Routes>
-                </RecoilRoot>
             </BrowserRouter>
         </div>
 
